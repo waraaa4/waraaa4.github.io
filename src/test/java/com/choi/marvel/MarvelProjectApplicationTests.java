b6cd6fa9c8dc1c4d916344a87c671eb48d023d9b
waraaa4 <warcjh00@naver.com>
2022-02-22 12:32:21 +0900

    Initial commit

---
+++ src/test/java/com/choi/marvel/MarvelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
